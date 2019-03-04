fef45435d27d271167eedd1ceb3acb0e916677bb
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-03-04 14:57:17 +0100

    Remove unnecessary things

--- src/App.js
+++ src/App.js
@@ -8,21 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Editkoa <code>src/App.js</code> and save to reload.
-          </p>
-          
-            <TodoItem />
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
       </div>
     );

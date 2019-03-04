640b5d0ca207d101e9a3f5f7e06cac6220aad99f
Wojciech Borkowski <wojciechborkowski13@gmail.com>
2019-03-04 16:43:47 +0100

    add ListItem component and some styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import ListItem from './components/ListItem.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+          <div className="todo-list">
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+          </div>
         </header>
       </div>
     );
---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ListItem(){
+    return(
+        <div className="list-item">
+            <input type="checkbox"></input>
+            <p>Placeholder text</p>
+        </div>
+    )
+}
+
+export default ListItem;
